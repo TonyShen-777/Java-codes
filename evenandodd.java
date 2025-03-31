fc4a7b996fd9cfd03c8d7af717a2e2ec9e3d2034
TonyShen-777 <hengyushen0@gmail.com>
2025-03-31 13:45:41 -0400

    Add files via upload

---
+++ evenandodd.java
@@ -0,0 +1,24 @@
+package applications;
+import java.util.Scanner;
+
+public class evenandodd {
+
+	public static void main(String[]args){
+
+	
+	        Scanner scanner = new Scanner(System.in);
+
+	      
+	        System.out.print("Enter a number: ");
+	        int number = scanner.nextInt();
+
+	     
+	        if (number % 2 == 0) {
+	            System.out.println(number + " is even.");
+	        } else {
+	            System.out.println(number + " is odd.");
+	        }
+
+
+	    }
+}
